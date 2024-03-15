6a06d9976b8e33aa069507862a805705bce8198a
SF4NU <mariusmarius200121@gmail.com>
2024-03-15 11:37:29 +0100

    finito lezione 11

---
+++ vue-lesson-11-component-event/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ComponentEvent from "./Components/ComponentEvent.vue";
+import FromComponent from "./Components/FromComponent.vue";
+import { ref } from "vue";
+
+let count = ref(0);
+
+const formHandler = (username, email, password) => {
+  console.log("username", username);
+  console.log("email", email);
+  console.log("password", password);
+};
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <ComponentEvent @incrementCounter="count++" />
+  <hr />
+  <FromComponent @userInfo="formHandler" />
+</template>
+
+<style scoped></style>
