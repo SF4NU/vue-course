1c36c14b35c758f33b8bd80716ca6b4f4f3f1dc6
SF4NU <mariusmarius200121@gmail.com>
2024-03-15 11:58:29 +0100

    finito lezione 12

--- vue-lesson-12-slot/src/App.vue
+++ vue-lesson-12-slot/src/App.vue
@@ -1,5 +1,32 @@
-<script setup></script>
+<script setup>
+import SlotComponent from "./Components/SlotComponent.vue";
+import FallbackContent from "./Components/FallbackContent.vue";
+import NamedSlotComponent from "./Components/NamedSlotComponent.vue";
+import DefaultSlot from "./Components/DefaultSlot.vue";
+</script>
 
-<template></template>
+<template>
+  <SlotComponent>
+    <h1>Content 1</h1>
+    <h1>Content 12</h1>
+  </SlotComponent>
+  <hr />
+  <FallbackContent>
+    Fallback wont work because i am providing content inside the component
+  </FallbackContent>
+
+  <hr />
+
+  <NamedSlotComponent>
+    <template v-slot:one> <h1>This content will go to slot 1</h1></template>
+    <template v-slot:two> <h1>This content will go to slot 2</h1></template>
+  </NamedSlotComponent>
+
+  <DefaultSlot>
+    <template #default>
+      <h1>This is some default content</h1>
+    </template>
+  </DefaultSlot>
+</template>
 
 <style scoped></style>
