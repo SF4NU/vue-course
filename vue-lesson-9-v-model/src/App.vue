2fd33c6beb274ea8ba75481344446fe9a5f2f7e2
SF4NU <mariusmarius200121@gmail.com>
2024-03-15 10:16:40 +0100

    finito lezione 9

---
+++ vue-lesson-9-v-model/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ModelComponent from "./Components/ModelComponent.vue";
+</script>
+
+<template>
+  <ModelComponent />
+</template>
+
+<style scoped></style>
---
+++ vue-lesson-9-v-model/src/Components/ModelComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+const formData = ref({ username: "", email: "", password: "" });
+</script>
+
+<template>
+  <form @submit.prevent>
+    <input
+      type="text"
+      placeholder="Please enter your name"
+      v-model="formData.username" />
+    <input
+      type="text"
+      placeholder="Please enter email"
+      v-model="formData.email" />
+    <input
+      type="text"
+      placeholder="Please enter password"
+      v-model="formData.password" />
+    <button type="submit">submit</button>
+  </form>
+
+  <h1>{{ formData.username }}</h1>
+  <h1>{{ formData.email }}</h1>
+  <h1>{{ formData.password }}</h1>
+</template>
